96858164d88a32c4de1cdeaaab318bf01d69efa0
Stephen O'Loughlin <soloughl@ucsd.edu>
2018-08-06 16:32:40 -0800

    statuschecking works

--- Assets/Scripts/ParseController.cs
+++ Assets/Scripts/ParseController.cs
@@ -17,12 +17,15 @@ public class ParseController : MonoBehaviour {
     public InputField passwordSignup;
     public InputField RetypePassword;
     string AcctName;
-    private static ParseObject AcctObj;
+    private static string AcctId;
+    private static string SessionToken;
 
     private static bool AccountFound = false;
     private static bool UsernameFound = false;
     private static bool LoggedIn = false;
     private static bool MembershipFound = false;
+    private static bool CurrentSessionFound = true;
+    private static bool StandingChecked = true;
 
     private static bool NextStep = false;
 
@@ -30,22 +33,34 @@ public class ParseController : MonoBehaviour {
     private static bool UsernameQueryFailed = false;
     private static bool LoginFailed = false;
     private static bool MembershipQueryFailed = false;
+    private static bool BadStanding = false;
 
-	// Use this for initialization
-	void Start () {
 
-        AcctName = "eXpTest";
-
+    private void Awake()
+    {
         ParseInitializeBehaviour _script = new GameObject("ParseInitializeBehaviour").AddComponent<ParseInitializeBehaviour>();
         _script.applicationID = "zquCagFDNXC8ipCFPjRjV8xw7y4Jik";
         _script.dotnetKey = "o3UfuztDWwYvE8GBgQRMewbd";
+    }
+
+
+    // Use this for initialization
+    void Start () {
+
+        AcctName = "WayMakers";
+
+        if(AcctName.Equals("WayMakers"))
+        {
+            CurrentSessionFound = false;
+            StandingChecked = false;
+        }
 
         ParseClient.Initialize(new ParseClient.Configuration
         {
             ApplicationId = "zquCagFDNXC8ipCFPjRjV8xw7y4Jik",
             WindowsKey = "o3UfuztDWwYvE8GBgQRMewbd",
 
-            Server = "http://staging.api.virbela.com/parse/"
+            Server = "https://api.virbela.com/parse/"
         });
 
         if(ParseUser.CurrentUser != null)
@@ -72,7 +87,7 @@ public class ParseController : MonoBehaviour {
     {
         Debug.Log("Querying username...");
         ParseQuery<ParseUser> query = ParseUser.Query.WhereEqualTo("username", usernameLogin.text);
-        query.CountAsync().ContinueWith(t =>
+        query.Limit(1).CountAsync().ContinueWith(t =>
         {
             if (t.Result == 0)
             {
@@ -113,14 +128,20 @@ public class ParseController : MonoBehaviour {
         ParseQuery<ParseObject> query = ParseObject.GetQuery("ServerConfig").WhereEqualTo("name", AcctName);
         query.FirstAsync().ContinueWith(t =>
         {
+            Debug.Log("Task entered");
             ParseObject ServerConfigObj = t.Result;
+            Debug.Log("ServerConfigObj set");
             if(!ServerConfigObj.ContainsKey("account"))
             {
+                Debug.Log("Entered if statement");
                 AccountQueryFailed = true;
             }
             else
             {
-                ServerConfigObj.TryGetValue<ParseObject>("account", out AcctObj);
+                Debug.Log("Entered else statement");
+                ParseObject temp;
+                ServerConfigObj.TryGetValue<ParseObject>("account", out temp);
+                AcctId = temp.ObjectId;
                 AccountFound = true;
                 NextStep = true;
             }
@@ -130,8 +151,12 @@ public class ParseController : MonoBehaviour {
     public void FindMembership()
     {
         Debug.Log("Finding membership...");
-        ParseQuery<ParseObject> query = ParseObject.GetQuery("Membership").WhereEqualTo("account", AcctObj).WhereEqualTo("user", ParseUser.CurrentUser);
-        query.CountAsync().ContinueWith(t =>
+        Debug.Log(AcctId);
+        Debug.Log(ParseUser.CurrentUser.ObjectId);
+        ParseQuery<ParseObject> query = ParseObject.GetQuery("Membership")
+            .WhereEqualTo("account", ParseObject.CreateWithoutData("Account", AcctId))
+            .WhereEqualTo("user", ParseUser.CreateWithoutData("_User", ParseUser.CurrentUser.ObjectId));
+        query.Limit(1).CountAsync().ContinueWith(t =>
         {
             if(t.Result == 0)
             {
@@ -141,6 +166,43 @@ public class ParseController : MonoBehaviour {
             {
                 MembershipFound = true;
                 NextStep = true;
+            }
+        });
+    }
+
+    public void GetCurrentSession()
+    {
+        Debug.Log("Getting Session token...");
+        ParseSession.GetCurrentSessionAsync().ContinueWith(t =>
+        {
+            var result = t.Result;
+            SessionToken = result.SessionToken;
+            Debug.Log(SessionToken);
+            CurrentSessionFound = true;
+            NextStep = true;
+        });
+
+    }
+
+    public void CheckStanding()
+    {
+        Debug.Log("Checking standing...");
+        IDictionary<string, object> dictionary = new Dictionary<string, object>
+        {
+            { "accountId", AcctId },
+            { "sessionToken", SessionToken }
+        };
+        ParseCloud.CallFunctionAsync<IDictionary<string, object>>("isSubscribed", dictionary).ContinueWith(t =>
+        {
+            foreach (var item in t.Result)
+            {
+                if (item.Key.Equals("isSubscribed"))
+                {
+                    NextStep = true;
+                    StandingChecked = item.Value.ToString().Equals("True");
+                    BadStanding = !StandingChecked;
+                    break;
+                }
             }
         });
     }
@@ -200,7 +262,17 @@ public class ParseController : MonoBehaviour {
             NextStep = false;
             FindMembership();
         }
-        else if (UsernameFound && LoggedIn && AccountFound && MembershipFound && NextStep)
+        else if (UsernameFound && LoggedIn && AccountFound && MembershipFound && NextStep && !CurrentSessionFound && !StandingChecked)
+        {
+            NextStep = false;
+            GetCurrentSession();
+        }
+        else if (UsernameFound && LoggedIn && AccountFound && MembershipFound && NextStep && CurrentSessionFound && !StandingChecked)
+        {
+            NextStep = false;
+            CheckStanding();
+        }
+        else if (UsernameFound && LoggedIn && AccountFound && MembershipFound && NextStep && CurrentSessionFound && StandingChecked)
         {
             NextStep = false;
             AccountFound = false;
@@ -221,28 +293,36 @@ public class ParseController : MonoBehaviour {
             }
         }
 
-        if(AccountQueryFailed)
-        {
-            Debug.Log("Account verification failed");
-            AccountQueryFailed = false;
-        }
-
-        if(LoginFailed)
+        if (LoginFailed)
         {
             Debug.Log("Invalid password");
             LoginFailed = false;
+        }
+
+        if (AccountQueryFailed)
+        {
+            Debug.Log("Account verification failed");
+            AccountQueryFailed = false;
+            ParseUser.LogOutAsync();
         }
 
         if(MembershipQueryFailed)
         {
             Debug.Log("Membership Query Failed");
             MembershipQueryFailed = false;
+            ParseUser.LogOutAsync();
         }
 
-        if(UsernameQueryFailed)
+        if (UsernameQueryFailed)
         {
             Debug.Log("Invalid username");
             UsernameQueryFailed = false;
+        }
+
+        if (BadStanding)
+        {
+            Debug.Log("Member is in bad standing");
+            BadStanding = false;
         }
 
     }
